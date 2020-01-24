328e11f68022d288db92e34a6fb958fddd26b831
Andrew Binnie <binnieandrew@gmail.com>
2020-01-24 13:54:33 +0000

    adding the first test

---
+++ tests/integration/tests.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+// grab elements click the buttons you want and then check running total equals what you want
+// run selenium, express server and run protractor
+// for number 5 will need to change the effect of the calculator and test it so the answer is not infinity
+
+});
---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,46 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('it can add two numbers', function(){
+    calculator.previousTotal = "1"
+    calculator.add("4")
+    assert.equal(5.0, calculator.runningTotal)
+  })
+  
+  // it('it can subtract two numbers', function(){
+  //   calculator.previousTotal = "7"
+  //   calculator.subtract("4")
+  //   assert.equal(3.0, calculator.runningTotal)
+  // })
+  //
+  // it('it can multiply two numbers', function(){
+  //   calculator.previousTotal = "3"
+  //   calculator.multiply("5")
+  //   assert.equal(15.0, calculator.runningTotal)
+  // })
+  //
+  // it('it can divide two numbers', function(){
+  //   calculator.previousTotal = "21"
+  //   calculator.divide("7")
+  //   assert.equal(3.0, calculator.runningTotal)
+  // })
+  //
+  // it('it can concatenate multiple numbers', function(){
+  //   calculator.runningTotal = "3.0"
+  //   calculator.numberClick("7.0")
+  //   assert.equal(73.0, calculator.runningTotal)
+  // })
+
+// what do you want to arrange, act, assert
+
+});
