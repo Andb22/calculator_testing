e4c4f4047787fb407c3f5706cc24167800ebf646
Andrew Binnie <binnieandrew@gmail.com>
2020-01-25 17:50:18 +0000

    adding the seventh unit test and first integration test

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,8 +16,17 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should have working arithmetical operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('10')
+  })
+
 // grab elements click the buttons you want and then check running total equals what you want
 // run selenium, express server and run protractor
 // for number 5 will need to change the effect of the calculator and test it so the answer is not infinity
-
+// submission is under canvas pda site
 });
--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -20,28 +20,44 @@ describe('calculator', function () {
   it('it can subtract two numbers', function(){
     calculator.previousTotal = "7"
     calculator.subtract("4")
-    assert.equal(3.0, calculator.runningTotal)
+    assert.strictEqual(3.0, calculator.runningTotal)
   })
 
   it('it can multiply two numbers', function(){
     calculator.previousTotal = "3"
     calculator.multiply("5")
-    assert.equal(15.0, calculator.runningTotal)
+    assert.strictEqual(15.0, calculator.runningTotal)
   })
 
   it('it can divide two numbers', function(){
     calculator.previousTotal = "21"
     calculator.divide("7")
-    assert.equal(3.0, calculator.runningTotal)
+    assert.strictEqual(3.0, calculator.runningTotal)
   })
 
   it('it can concatenate multiple numbers', function(){
     calculator.runningTotal = "3"
     calculator.newTotal = false
     calculator.numberClick("7")
-    assert.equal(37.0, calculator.runningTotal)
+    assert.strictEqual(37.0, calculator.runningTotal)
   })
 
+  it('it can chain multiple operations', function(){
+    calculator.previousTotal = "3"
+    calculator.add("9")
+    calculator.previousOperator = "+"
+    calculator.operatorClick("+")
+    assert.strictEqual(15.0, calculator.previousTotal)
+  })
+
+
+  it('it can clear the running total without affecting the calculation', function(){
+    calculator.previousTotal = "10"
+    calculator.runningTotal = "3"
+    calculator.clearClick()
+    calculator.add("15")
+    assert.strictEqual(25.0, calculator.runningTotal)
+  })
 // what do you want to arrange, act, assert
 
 });
