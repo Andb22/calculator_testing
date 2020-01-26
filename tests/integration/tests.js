683352fa287373bd4010f69fce005e77cedb8ead
Andrew Binnie <binnieandrew@gmail.com>
2020-01-26 14:16:45 +0000

    adding the integration test for very large numbers and exceptional data

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -35,6 +35,57 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('30')
   })
+
+  it('should be able to output positive numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('40')
+  })
+
+  it('should be able to output negative numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+  })
+
+  it('should be able to output decimal numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5')
+  })
+
+  it('should be able to output very large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('99980001')
+  })
+
+  it('should be able to output undefined when dividing by 0', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('undefined')
+  })
 // grab elements click the buttons you want and then check running total equals what you want
 // run selenium, express server and run protractor
 // for number 5 will need to change the effect of the calculator and test it so the answer is not infinity
